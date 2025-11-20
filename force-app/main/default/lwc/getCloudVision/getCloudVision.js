90fd4e3e1b78df0443d0238f150bf97efda83121
dubeyabhishek1005 <dubeyabhishek1005@gmail.com>
2025-11-20 07:39:08 +0530

    adding one more way to filter the JSON response (JSON.deserialize)

--- force-app/main/default/lwc/getCloudVision/getCloudVision.js
+++ force-app/main/default/lwc/getCloudVision/getCloudVision.js
@@ -1,9 +1,11 @@
 import { LightningElement, wire } from 'lwc';
 import getCloudVisionResponse from '@salesforce/apex/googleCloudVisionController.getCloudVisionResponse';
+import getCloudVisionResponseJSON from '@salesforce/apex/googleCloudVisionController.getCloudVisionResponseJSON';
 
 export default class GetCloudVision extends LightningElement {
 
     data = [];
+    dataJSON = [];
     
     handleCloudVisionResponse(){
         getCloudVisionResponse()
@@ -13,6 +15,18 @@ export default class GetCloudVision extends LightningElement {
         })
         .catch( (error) => {
             this.data = error;
+            console.log('ERROR',error);
+        });
+    }
+
+    handleCloudVisionResponseJSON(){
+        getCloudVisionResponseJSON()
+        .then( (response) => {
+            this.dataJSON = response;
+            console.log('SUCESS',response);
+        })
+        .catch( (error) => {
+            this.dataJSON = error;
             console.log('ERROR',error);
         });
     }
